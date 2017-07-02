ee61fc75d5e35f5e8088bdeb3e07a3767eeffd29
Jim Pick <jim@backblend.com>
2017-07-02 11:23:21 -0700

    Start moving to async/await
    Not sure why I didn't think it worked initially.

--- index.js
+++ index.js
@@ -8,45 +8,58 @@ app.use(store)
 app.route('/', listView)
 app.mount('ul#siteList')
 
-function store (state, emitter) {
+async function store (state, emitter) {
   state.sites = []
-  function update() {
-    fetch('/sites.json')
-      .then(response => response.json())
-      .then(sites => {
-        state.sites = sites
-        emitter.emit('render')
-      })
+  async function update () {
+    const response = await fetch('/sites.json')
+    const sites = await response.json()
+    state.sites = sites
+    emitter.emit('render')
   }
-  update() // Initial load
+  await update() // Initial load
   emitter.on('update', update)
 }
 
-const buttonEl = document.querySelector('#createDat')
-
-if (!window.DatArchive) {
-  buttonEl.remove()
-  publishButtonEl.remove()
-} else {
-  const url = 'dat://188e04da36894f212254b72d9c7e0d0fb6cb12574eabf2761d268cfe618d75aa/'
-  const archive = new Archive(url)
-  archive.init()
-    .then(() => {
-      function clickHandler(state, emitter) {
-        buttonEl.addEventListener('click', event => {
-          const now = Date.now()
-          const file = `/sites/${now}.json`
-          const newArchive = new NewArchive()
-          const defaultTitle = `Title ${now}`
-          newArchive.create(defaultTitle)
-            .then(info => url && archive.writeFile(file, info))
-            .then(() => emitter.emit('update'))
-            .catch(error => {
-              console.log('Create error', error)
-            })
-        })
+const makeClickHandler = (archive) => {
+  const func = (_, emitter) => {
+    const buttonEl = document.querySelector('#createDat')
+    buttonEl.addEventListener('click', async event => {
+      const now = Date.now()
+      const file = `/sites/${now}.json`
+      try {
+        const newArchive = new NewArchive()
+        const defaultTitle = `Title ${now}`
+        const info = await newArchive.create(defaultTitle)
+        await archive.writeFile(file, info)
+        emitter.emit('update')
+      } catch (error) {
+        console.log('Create error', error)
       }
-      app.use(clickHandler)
     })
+  }
+  return func
 }
 
+async function run () {
+  const url = document.location.href
+  const archive = new Archive(url)
+  await archive.init()
+  app.use(makeClickHandler(archive))
+}
+
+if (!window.DatArchive) {
+  const buttonEl = document.querySelector('#createDat')
+  buttonEl.remove()
+} else {
+  run()
+}
+
+function timer() {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('Timer finished')
+      resolve()
+    }, 1000)
+  })
+  return promise
+}
